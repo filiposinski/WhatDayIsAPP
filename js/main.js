379ca3ac99bc730b78066d884ef7f4fc9cbc5409
spacehiker <filip.osin@gmail.com>
2020-04-21 09:43:44 +0200

    setup pwa

---
+++ js/main.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    'use strict';
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./sw.js');
+    }
+}
+
+const button = document.querySelector('.change--js');
+let date = new Date();
+let day = date.getDay();
+let dayNum = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+
+console.log(day);
+
+button.addEventListener('click', write)
+
+
+function write() {
+    button.innerHTML = `Today is ${week[day]} <br> ${dayNum}/${month}/${year}`;
+}
